fb544de832575adafc69335bb09d997b8aded373
syedmuzammil797 <syedmuzammilmm3@gmail.com>
2022-10-07 22:59:55 -0400

    file2 updated

--- Muzammil.java
+++ Muzammil.java
@@ -2,6 +2,8 @@ public class Muzammil {
     public static void main (String[] args){
         System.out.println("Hello Muzammil");
         System.out.println("hey minhaj ");
+        System.out.println("hey faiz , how r u");
+        System.out.println("faiz: iam good ");
     }
     
 }
